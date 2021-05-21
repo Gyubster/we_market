b21103c62009b73c89c1517475ca7a64c0208b49
Gyubster <kyunbo0910@naver.com>
2021-05-21 23:23:34 +0900

    CREATE: post detail

--- api/post/serializers.py
+++ api/post/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework     import serializers
 
+from user.models    import User
 from post.models    import Post, PostImage
 
 class PostSerializer(serializers.ModelSerializer):
@@ -18,3 +19,27 @@ class PostListSerializer(serializers.ModelSerializer):
     class Meta:
         model   = Post
         fields  = ['title', 'image','address', 'created_at', 'price', 'like_count', 'chat_count', 'product']
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    images      = ImageSerializer(many=True, read_only=True)
+    subcategory = serializers.ReadOnlyField(source='subcategory.name')
+    writer      = serializers.SerializerMethodField()
+
+    class Meta:
+        model       = Post
+        fields      = ['id', 'title', 'address', 'created_at', 'price', 'like_count', 'chat_count', 'images', 'subcategory', 'possible_discount', 'view_count', 'introduction', 'writer']
+
+    def get_writer(self, obj):
+        user = User.objects.get(id=obj.user_id)
+        writer = {
+                'writer_nickname'       : user.nickname,
+                'wirter_profile_image'  : user.profile_image,
+                'writer_posts'          : [
+                    {
+                        'title'         : post.title,
+                        'post_image'    : post.images.first().url,
+                        'price'         : post.price
+                    } for post in Post.objects.filter(user_id=user.id)]
+            }
+        return writer
+    
